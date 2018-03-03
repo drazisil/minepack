ba5caa3a348957c2dce00e1185c591b45c101eeb
Joseph W Becher <jwbecher@drazisil.com>
2018-03-03 08:46:50 -0500

    fetch and decompess the modlist file

--- minepack.py
+++ minepack.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python3
 
+from bz2 import decompress
 import getpass
+from io import BytesIO
+from os import mkdir
+from os.path import exists
+import requests
 
-username = input("What is your Twitch username? ")
-password = getpass.getpass()
+def getTwitchLogin():
+  username = input("What is your Twitch username? ")
+  password = getpass.getpass()
+
+  return username, password
+
+def createCacheDirectory(cacheDirectyPath):
+  
+  if exists(cacheDirectyPath):
+    print('Cache directory already exists')
+  else:
+    print('Cache directory does not exist, creating it')
+    mkdir(cacheDirectyPath)
+
+def updateModListJSON(curseCompleteModListURL, modListJSONPath):
+  if exists(modListJSONPath):
+    print('Mod list JSON already exists')
+  else:
+    print('Mod list JSON not exist, fetching it')
+    r = requests.get(curseCompleteModListURL)
+    with open(modListJSONPath, 'wb') as f:
+      f.write(decompress(r.content))
+    
+
+curseCompleteModListURL = "http://clientupdate-v6.cursecdn.com/feed/addons/432/v10/complete.json.bz2"
+modListJSONPath = 'cache/complete.json'
+
+def main():
+  createCacheDirectory('cache')
+  updateModListJSON(curseCompleteModListURL, modListJSONPath)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
